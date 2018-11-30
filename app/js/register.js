77c51646d6f9d631b6ddfac5eefc8f2b154047ec
453228438 <m18148759938_1@163.com>
2018-11-30 20:03:52 +0800

    获取了所有的dom元素，用的jQuery的选择器$

--- app/js/register.js
+++ app/js/register.js
@@ -12,3 +12,24 @@ var checkInput = {
         return reg.test(str);
     },
 }
+
+var register = (function() {
+    return {
+        init: function(ele) {
+            this.$box = $(ele);
+            this.$phone = $('.phone'); //获取手机号输入框
+            this.$psw = $('.psw'); //获取密码输入框
+            this.$psw2 = $('.psw2'); //获取确认密码输入框
+            this.$ranImg = $('im'); //获取随机图片字符输入框
+            this.$information = $('.information2'); //获取短信验证输入框
+            this.$txt = $('.showTxt'); //获取验证错误提示语框
+            this.$regBtn = $('.regBtn'); //获取注册按钮
+            this.event();
+        },
+        event() {
+            var that = this;
+        },
+    }
+})()
+
+register.init('.page');
