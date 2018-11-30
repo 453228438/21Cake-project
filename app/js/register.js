312973236dd3bd6930d055afd434eab69354667e
453228438 <m18148759938_1@163.com>
2018-11-30 20:57:03 +0800

    写了验证phone和password的功能，但是毫无效果= =，找不到原因

--- app/js/register.js
+++ app/js/register.js
@@ -1,8 +1,8 @@
 var checkInput = {
-    username(str) {
-        var reg = /^\w{6,20}$/;
-        return reg.test(str);
-    },
+    // username(str) {
+    //     var reg = /^\w{6,20}$/;
+    //     return reg.test(str);
+    // },
     password(str) {
         var reg = /^\w{6,20}$/;
         return reg.test(str);
@@ -24,10 +24,31 @@ var register = (function() {
             this.$information = $('.information2'); //获取短信验证输入框
             this.$txt = $('.showTxt'); //获取验证错误提示语框
             this.$regBtn = $('.regBtn'); //获取注册按钮
+            this.inpArr = [];
+            this.inpArr.push(this.$psw);
+            this.inpArr.push(this.$phone);
+            //console.log(this.inpArr);
             this.event();
         },
         event() {
             var that = this;
+            var phone = this.inpArr[1];
+            // debugger;
+            for (let i = 0; i < this.inpArr.length; i++) {
+                this.inpArr[i].onblur = function() {
+                    if (this.value == '') {
+                        this.$txt.innerHTML = '输入不能为空';
+                    } else {
+                        var bool = checkInput[this.name](this.value);
+                        if (bool) {
+                            this.$txt.className = 'showTxt2';
+                            this.$txt.innerHTML = '验证成功';
+                        } else {
+                            this.$txt.innerHTML = '您的输入有误';
+                        }
+                    }
+                }
+            }
         },
     }
 })()
