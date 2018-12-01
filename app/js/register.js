a0fcc908e3656c977b55ba6421ec647f36f2f147
453228438 <m18148759938_1@163.com>
2018-12-01 17:22:09 +0800

    我竟然摸索着就把图片验证的功能写好了，包括点击刷新，以及输入有误后，再次获取焦点就自动刷新，好棒棒~

--- app/js/register.js
+++ app/js/register.js
@@ -3,14 +3,21 @@ var checkInput = {
     //     var reg = /^\w{6,20}$/;
     //     return reg.test(str);
     // },
-    password(str) {
-        var reg = /^\w{6,20}$/;
-        return reg.test(str);
-    },
     phone(str) {
         var reg = /^1[35789]\d{9}$/;
         return reg.test(str);
     },
+    password(str) {
+        var reg = /^\w{6,20}$/;
+        return reg.test(str);
+    }
+    // im(str) {
+    //     var reg = document.querySelector('.ran_im').innerHTML;
+    //     console.log(reg);
+    //     return str == reg;
+
+    // }
+
 }
 
 var register = (function() {
@@ -20,19 +27,22 @@ var register = (function() {
             this.$phone = this.$box.querySelector('.phone'); //获取手机号输入框
             this.$psw = this.$box.querySelector('.psw'); //获取密码输入框
             this.$psw2 = this.$box.querySelector('.psw2'); //获取确认密码输入框
-            this.$ranImg = this.$box.querySelector('im'); //获取随机图片字符输入框
+            this.$ranImg = this.$box.querySelector('.im'); //获取随机图片字符输入框
             this.$information = this.$box.querySelector('.information2'); //获取短信验证输入框
             this.$txt = this.$box.querySelector('.showTxt'); //获取验证错误提示语框
             this.$regBtn = this.$box.querySelector('.regBtn'); //获取注册按钮
-            this.inpArr = [];
-            this.inpArr.push(this.$psw);
-            this.inpArr.push(this.$phone);
-            //console.log(this.inpArr);
+            this.inpArr = this.$box.querySelectorAll('input');
+            this.$im = this.$box.querySelector('.ran_im');
+            // this.inpArr.push(this.$psw);
+            // this.inpArr.push(this.$phone);
+            console.log(this.inpArr);
             this.event();
+            this.setRanNum();
+            // this.checkInput2(str);
         },
         event() {
             var that = this;
-            var phone = this.inpArr[1];
+            var phone = this.inpArr[0];
             // debugger;
             for (let i = 0; i < this.inpArr.length; i++) {
                 this.inpArr[i].onblur = function() {
@@ -73,6 +83,44 @@ var register = (function() {
                 }
                 sendAjax('../server/php/register.php', obj);
             }
+            this.$im.onclick = function() { //点击图片字符，就刷新字符
+                that.setRanNum();
+            }
+            this.$ranImg.onblur = function() { //判断输入的字符是否与图片中的符合
+                if (this.value == '') {
+                    that.$txt.style.color = '#ff714d';
+                    that.$txt.innerHTML = '输入不能为空';
+                } else {
+                    var bool = that.checkInput2(this.value);
+                    if (bool) {
+                        that.$txt.style.color = 'green';
+                        that.$txt.innerHTML = '验证成功';
+                    } else {
+                        that.$txt.style.color = '#ff714d';
+                        that.$txt.innerHTML = '您的输入有误';
+                        this.onfocus = function() { //如果输入有误，重新获取焦点后刷新字符图片
+                            that.setRanNum();
+                        }
+                    }
+                }
+            }
+        },
+        setRanNum() {
+            var that = this;
+            var ranArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'];
+            var tt = '';
+            for (let i = 0; i < 4; i++) {
+                var ran = getRandom(ranArr.length - 1, 0);
+                tt += ranArr[ran];
+            }
+            that.$im.innerHTML = tt;
+            // console.log(that.$im.innerHTML);
+        },
+        checkInput2(str) {
+            var that = this;
+            var reg = this.$im.innerHTML;
+            return str == reg;
+
         },
     }
 })()
