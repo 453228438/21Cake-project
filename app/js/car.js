b60619389025a6344a06c07401d94715695241ef
shengjinghua <2585990943@qq.com>
2018-12-11 21:53:56 +0800

    更新了购物车渲染

---
+++ app/js/car.js
@@ -0,0 +1,90 @@
+var car = (function () {
+    return {
+        init() {
+            this.event();
+            this.getData()
+        },
+        event() {
+
+        },
+        getData() {
+            var shopList = localStorage.shopList;
+            shopList = JSON.parse(shopList)
+            this.data = shopList;
+            this.insertData(shopList)
+        },
+        insertData(data) {
+            console.log(data)
+            $('.mainInner').children().remove()
+            $('.mainInner').css({
+                'padding-top': '100px'
+            })
+            $('.mainInner').append('<div class="list"><h2>商品</h2><h2>生日牌</h2><h2>单价</h2><h2>数量</h2><h2>金额</h2></div>')
+            $('.mainInner .list').css({
+                'width': '1195px',
+                'height': '46px',
+                'border': '1px solid #e1e1e1',
+                'backgroundColor': '#fafafa',
+                'boxSizing': 'border-Box'
+            })
+            $('.mainInner .list h2').css({
+                'float': 'left',
+                'fontSize': '14px'
+            })
+            $('.mainInner .list h2').eq(0).css({
+                'marginLeft': '166px'
+            })
+            $('.mainInner .list h2').eq(1).css({
+                'marginLeft': '260px'
+            })
+            $('.mainInner .list h2').eq(2).css({
+                'marginLeft': '260px'
+            })
+            $('.mainInner .list h2').eq(3).css({
+                'marginLeft': '88px'
+            })
+            $('.mainInner .list h2').eq(4).css({
+                'marginLeft': '124px'
+            })
+            $('.mainInner').append('<table></table>')
+            data.forEach((item, index) => {
+                $('.mainInner table').append('<tr><td><a href=""><div></div></a></td><td><section><h2><a href=""></a></h2><span class="kg"></span><span class="tableware"><i></i>赠送5套餐具</span></section></td><td><select><option value ="volvo">选择生日牌</option><option value ="volvo">生日快乐</option><option value ="volvo">Happly Birthday</option><option value ="volvo">节日快乐</option></select></td><td><h2></h2></td><td><main class="num"><input type="button" class="left" value="-" /><input type="text" class="text"/><input type="button" class="right" value="+" /></main></td><td><h2 class="meony"></h2></td><td><button type="button" class="delete">删除商品</button></td></tr>');
+                $('.mainInner table tr').find('div').eq(index).css({
+                    'backgroundImage': 'url(' + item.banner1 + ')'
+                })
+                $('.mainInner table tr td:nth-child(2) section h2 a').eq(index).html(item.title)
+                $('.mainInner table tr td:nth-child(2) section .kg').eq(index).html('规格 :' + item.kg)
+                $('table tr td:nth-child(4) h2').eq(index).html(item.picle)
+                $('table tr td:nth-child(5) .num .text').eq(index).val(item.count)
+                $('table tr td:nth-child(6) .meony').eq(index).html((item.count * item.num) + '元')
+            });
+            $('table tr td:nth-child(5) .num .text').blur(function () {
+                $index = $(this).parent().parent().parent().index();
+                data[$index].count = $(this).val() - 0;
+                $(this).parent().parent().next().children('.meony').html((data[$index].count * data[$index].num) + '元')
+                localStorage.shopList = JSON.stringify(data)
+            })
+            $('table tr td:nth-child(5) .num .left').click(function () {
+                $index = $(this).parent().parent().parent().index()
+                if ($(this).next().val() >= 2) {
+                    $(this).next().val($(this).next().val() - 1)
+                    data[$index].count -= 1;
+                    $(this).parent().parent().next().children('.meony').html((data[$index].count * data[$index].num) + '元')
+                    localStorage.shopList = JSON.stringify(data)
+                }
+            })
+            $('table tr td:nth-child(5) .num .right').click(function () {
+                $index = $(this).parent().parent().parent().index()
+                $(this).prev().val($(this).prev().val() - 0 + 1)
+                data[$index].count += 1;
+                $(this).parent().parent().next().children('.meony').html((data[$index].count * data[$index].num) + '元')
+                localStorage.shopList = JSON.stringify(data)
+            })
+
+
+
+
+        }
+    }
+}())
+car.init();
--- app/js/detallPages.js
+++ app/js/detallPages.js
@@ -1,223 +1,137 @@
- var detall = (function () {
-     var $banner = $('#wrapper-banner');
-     var $min = $('#warpper-main');
-     var $jiesuan = $('.jiesuan');
-     var $options = $('.options');
-     return {
-         init(i) {
-             console.log()
-             console.log()
-             this.event();
-             this.getData(i)
+function getUrl() {
+    var str = window.location.href;
+    //console.log(str);
+    var urlObj = {};
+    var index = str.indexOf("?");
+    var newStr = str.substr(index + 1);
+    var newStr2 = newStr.split("=");
+    return newStr2[1];
+}
+var detall = (function () {
+    var $banner = $('#wrapper-banner');
+    var $min = $('#warpper-main');
+    var $jiesuan = $('.jiesuan');
+    var $options = $('.options');
+    return {
+        init(i) {
+            console.log()
+            console.log()
+            this.event();
+            this.getData(i)
+        },
+        event() {
+            var _this = this;
+            $(document).scroll(function () {
+                if ($(document).scrollTop() >= $banner.innerHeight() + $min.innerHeight()) {
+                    $('.header').css({
+                        'display': 'none'
+                    })
+                    $('.details-nav').css({
+                        'display': 'block'
+                    })
+                } else {
+                    $('.header').css({
+                        'display': 'block'
+                    })
+                    $('.details-nav').css({
+                        'display': 'none'
+                    })
+                }
+            })
+            $('.min-banner').children('div').eq(0).siblings().css({
+                'border': 'none'
+            })
+            console.log()
+            $('.min-banner').children('div').hover(function () {
+                $(this).css({
+                    'border': '1px solid #C69C6D',
+                })
+                $('#wrapper-banner .big-banner').css({
+                    'backgroundImage': $(this).css("backgroundImage")
+                })
+                $(this).siblings().css({
+                    'border': 'none'
+                })
+            })
+            $jiesuan.hover(function () {
+                $options.slideDown()
+            })
+            $options.mouseleave(function () {
+                $options.slideUp()
+            })
+        },
+        getData(i) {
+            var _this = this;
+            $.ajax('http://localhost:8887/21Cake-project/server/json/detallPages.json', {
+                type: 'post',
+                contentType: 'application/json',
+                success(res) {
+                    _this.setItem(res, i)
+                }
+            })
+        },
+        setItem(res, i) {
+            res = res.data[i];
+            var List = [];
+            for (var i = 0; i < 4; i++) {
+                var shopList = {}
+                shopList.id = res.id;
+                shopList.title = res.title
+                shopList.banner1 = res.banner1
+                shopList.count = 0;
+                shopList.picle = null;
+                shopList.kg = null;
+                //  console.log(shopList)
+                List.push(shopList)
+                console.log(List)
+            }
+            this.upData(res, List)
+        },
+        upData(res, List) {
+            var arr = localStorage.shopList || '[]';
+            arr = JSON.parse(arr);
+            $('.btn-right').click(function () {
+                var i = $('.icon-chenggong').parent().index();
+                if (i == 1) {
+                    List[0].picle = res.picle;
+                    List[0].kg = res.kg1;
+                    List[0].num = res.num1;
+                    List[0].count += 1;
+                    arr.push(List[0]);
+                }
+                if (i == 2) {
+                    List[1].picle = res.picle2;
+                    List[1].kg = res.kg2;
+                    List[1].num = res.num2;
+                    List[1].count += 1;
+                    arr.push(List[1]);
+                }
+                if (i == 3) {
+                    List[2].picle = res.picle3;
+                    List[2].kg = res.kg3;
+                    List[2].num = res.num3;
+                    List[2].count += 1;
+                    arr.push(List[2]);
+                }
+                if (i == 4) {
+                    List[3].picle = res.picle4;
+                    List[3].kg = res.kg4;
+                    List[3].num = res.num4;
+                    List[3].count += 1;
+                    arr.push(List[3]);
+                }
+                for (var i = 0; i < arr.length; i++) {
+                    for (var j = i + 1; j < arr.length; j++) {
+                        if (arr[i].kg == arr[j].kg && arr[i].id == arr[j].id) {
+                            arr[i].count = arr[j].count;
+                            arr.splice(j, 1)
+                        }
+                    }
+                }
+                localStorage.shopList = JSON.stringify(arr)
+            })
+        }
 
-         },
-         event() {
-             var _this = this;
-             $(document).scroll(function () {
-                 if ($(document).scrollTop() >= $banner.innerHeight() + $min.innerHeight()) {
-                     $('.header').css({
-                         'display': 'none'
-                     })
-                     $('.details-nav').css({
-                         'display': 'block'
-                     })
-                 } else {
-                     $('.header').css({
-                         'display': 'block'
-                     })
-                     $('.details-nav').css({
-                         'display': 'none'
-                     })
-                 }
-             })
-             $('.min-banner').children('div').eq(0).siblings().css({
-                 'border': 'none'
-             })
-             console.log()
-             $('.min-banner').children('div').hover(function () {
-                 $(this).css({
-                     'border': '1px solid #C69C6D',
-                 })
-                 $('#wrapper-banner .big-banner').css({
-                     'backgroundImage': $(this).css("backgroundImage")
-                 })
-                 $(this).siblings().css({
-                     'border': 'none'
-                 })
-             })
-             $jiesuan.hover(function () {
-                 $options.slideDown()
-             })
-             $options.mouseleave(function () {
-                 $options.slideUp()
-             })
-         },
-         getData(i) {
-             var _this = this;
-             $.ajax('http://localhost:8887/21Cake-project/server/json/detallPages.json', {
-                 type: 'post',
-                 //  contentType: 'application/json',
-                 success(res) {
-                     console.log(res)
-                     _this.setItem(res, i)
-                 }
-             })
-         },
-         setItem(res, i) {
-             res = res.data[i];
-             console.log(res)
-             var List = [];
-             for (var i = 0; i < 4; i++) {
-                 var shopList = {}
-                 shopList.id = res.id;
-                 shopList.title = res.title
-                 shopList.banner1 = res.banner1
-                 shopList.count = 0;
-                 shopList.picle = null;
-                 shopList.kg = null;
-                 //  console.log(shopList)
-                 List.push(shopList)
-                 console.log(List)
-             }
-             this.upData(res, List)
-         },
-         upData(res, List) {
-             console.log(List)
-             var arr = '[]';
-             arr = JSON.parse(arr)
-
-             $('.btn-right').click(function () {
-                 $('div.number').each(function (i) {
-                     if ($('.icon-chenggong').parent().index() == i) {
-                         if (i == 1) {
-                             List[i - 1].picle = res.picle;
-                             List[i - 1].kg = res.kg1;
-                             List[i - 1].count += 1;
-                             if (List[i - 1].count == 1) {
-                                 arr.push(JSON.stringify(List[i - 1]))
-                             } else if (List[i - 1].count >= 2) {
-                                 for (let j = 0; j < arr.length; j++) {
-                                     if (typeof (arr[j]) == 'string') {
-                                         arr[j] = JSON.parse(arr[j])
-                                     }
-                                     if (arr[j].kg == List[i - 1].kg) {
-                                         arr[j].count = List[i - 1].count
-                                     }
-                                     arr[j] = JSON.stringify(arr[j])
-                                 }
-                             }
-                             localStorage.shopList = arr
-                         }
-                         if (i == 2) {
-                             List[i - 1].picle = res.picle2;
-                             List[i - 1].kg = res.kg2;
-                             List[i - 1].count += 1;
-                             if (List[i - 1].count == 1) {
-                                 flag2 = true
-                                 arr.push(JSON.stringify(List[i - 1]))
-                             } else if (List[i - 1].count >= 2) {
-                                 for (let j = 0; j < arr.length; j++) {
-                                     if (typeof (arr[j]) == 'string') {
-                                         arr[j] = JSON.parse(arr[j])
-                                     }
-                                     if (arr[j].kg == List[i - 1].kg) {
-                                         arr[j].count = List[i - 1].count
-                                     }
-                                     arr[j] = JSON.stringify(arr[j])
-                                 }
-                             }
-                             console.log(arr)
-                             localStorage.shopList = arr
-                         }
-                         if (i == 3) {
-                             List[i - 1].picle = res.picle3;
-                             List[i - 1].kg = res.kg3;
-                             List[i - 1].count += 1;
-                             if (List[i - 1].count == 1) {
-                                 flag3 = true
-                                 arr.push(JSON.stringify(List[i - 1]))
-                             } else if (List[i - 1].count >= 2) {
-                                 for (let j = 0; j < arr.length; j++) {
-                                     if (typeof (arr[j]) == 'string') {
-                                         arr[j] = JSON.parse(arr[j])
-                                     }
-                                     if (arr[j].kg == List[i - 1].kg) {
-                                         arr[j].count = List[i - 1].count
-                                     }
-                                     arr[j] = JSON.stringify(arr[j])
-                                 }
-                             }
-                             console.log(arr)
-                             localStorage.shopList = arr
-                         }
-                         if (i == 4) {
-                             List[i - 1].picle = res.picle4;
-                             List[i - 1].kg = res.kg4;
-                             List[i - 1].count += 1;
-                             console.log(List[i - 1])
-                             if (List[i - 1].count == 1) {
-                                 flag4 = true
-                                 arr.push(JSON.stringify(List[i - 1]))
-                             } else if (List[i - 1].count >= 2) {
-                                 for (let j = 0; j < arr.length; j++) {
-                                     if (typeof (arr[j]) == 'string') {
-                                         arr[j] = JSON.parse(arr[j])
-                                     }
-                                     if (arr[j].kg == List[i - 1].kg) {
-                                         arr[j].count = List[i - 1].count
-                                     }
-                                     arr[j] = JSON.stringify(arr[j])
-                                 }
-                                 console.log(arr)
-                                 localStorage.shopList = arr;
-                             }
-
-                         }
-                     }
-                 })
-
-             })
-             //  else {
-             //      console.log(JSON.parse(localStorage.shopList))
-             //      $('.btn-right').click(function () {
-             //          var i = $('.icon-chenggong').parent().index();
-             //          if (i == 1) {
-             //              console.log(arr2.length)
-             //              for (let i = 0; i < arr2.length; i++) {
-             //                  if (arr2[i].kg == res.kg1) {
-             //                      arr2[i].count += 1;
-             //                  }
-             //              }
-             //              localStorage.shopList = JSON.stringify(arr2)
-             //          }
-             //          if (i == 2) {
-             //              for (let i = 0; i < arr2.length; i++) {
-             //                  if (arr2[i].kg == res.kg2) {
-             //                      arr2[i].count += 1;
-             //                  }
-             //              }
-             //              localStorage.shopList = JSON.stringify(arr2)
-             //          }
-             //          if (i == 3) {
-             //              for (let i = 0; i < arr2.length; i++) {
-             //                  if (arr2[i].kg == res.kg3) {
-             //                      arr2[i].count += 1;
-             //                  }
-             //              }
-             //              localStorage.shopList = JSON.stringify(arr2)
-             //          }
-             //          if (i == 4) {
-             //              for (let i = 0; i < arr2.length; i++) {
-             //                  if (arr2[i].kg == res.kg4) {
-             //                      arr2[i].count += 1;
-             //                  }
-             //              }
-             //              localStorage.shopList = JSON.stringify(arr2)
-             //          }
-             //      })
-             //  }
-         }
-
-     }
- }());
- detall.init(1);
+    }
+}());
+detall.init(getUrl())
