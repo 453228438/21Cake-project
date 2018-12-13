9eb86d4108ebc15d8c9607e2a434e30ce8e2cd7a
453228438 <m18148759938_1@163.com>
2018-12-13 17:39:37 +0800

    终极版本

--- gulpfile.js
+++ gulpfile.js
@@ -13,135 +13,66 @@ const rev = require('gulp-rev') //添加后缀名
 var runSequence = require('run-sequence');
 var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
-// gulp.task('default', ['minijs', 'minicss', 'minihtml', 'watch', 'connect']);
-gulp.task('default', function(callback) {
-    runSequence('build-clean', ['minijs', 'minicss'],
-        'minihtml',
-        'watch',
-        'connect',
-        callback);
-});
-gulp.task('minijs', function() { //压缩并合并js
-        gulp.src('app/app2/*.js')
-            .pipe(babel({
-                presets: ['@babel/env']
-            }))
-            .pipe(uglify())
-            .pipe(rev()) //给压缩后的js文件生成hash字符串后缀
-            // .pipe(concat('all.js')) //合并js
-            .pipe(gulp.dest('dist')) //加入dist文件夹内
-            .pipe(rev.manifest()) //生成json文件，进行映射
-            .pipe(gulp.dest('rev/js')) //加入rev/js文件夹内
-            .pipe(connect.reload())
-    })
-    // 
-    // gulp.task('minihtml', function () { //压缩并合并html
-    //     gulp.src('')
-    //         .pipe(htmlmin())
-    //         .pipe(concat('all.html'))
-    //         .pipe(gulp.dest('dist'))
-    //         .pipe(connect.reload()); //发生改变刷新
-    // })
-    // gulp.task('minicss', function () { //压缩并合并css
-    //     gulp.src('')
-    //         .pipe(minifycss())
-    //         .pipe(concat('all.css'))
-    //         .pipe(gulp.dest('dist'))
-    //         .pipe(connect.reload())
-    // })
-    // gulp.task('miniimg', function () { //压缩并合并img
-    //     gulp.src('')
-    //         .pipe(imagemin())
-    //         // .pipe(concat('all.image'))
-    //         .pipe(gulp.dest('dist'))
-    //         .pipe(connect.reload())
-    // })
-    // gulp.task('watch', function () { //实时监听
-    //     gulp.watch('', ['minihtml'])
-    //     gulp.watch('', ['minijs'])
-    //     gulp.watch('', ['minicss'])
-    //     // gulp.watch('', ['miniimg'])
-    // })
-    // gulp.task('minihtml', function() {
-
-// gulp.task('default', function (callback) {
-//     runSequence('build-clean',
-//         ['minijs', 'minicss'],
-//         'minihtml',
-//         'watch',
-//         'connect',
-//         callback);
-// });
 
 gulp.task('minijs', function() { //压缩并合并js
-        gulp.src('app/app2/*.js')
-            .pipe(babel({
-                presets: ['@babel/env']
-            }))
-            .pipe(uglify())
-            .pipe(rev()) //给压缩后的js文件生成hash字符串后缀
-            // .pipe(concat('all.js')) //合并js
-            .pipe(gulp.dest('dist')) //加入dist文件夹内
-            .pipe(rev.manifest()) //生成json文件，进行映射
-            .pipe(gulp.dest('rev/js')) //加入rev/js文件夹内
-            .pipe(connect.reload())
-    })
-    // })
-    // gulp.task('minihtml', function () { //压缩并合并html
-    //     gulp.src('')
-    //         .pipe(htmlmin())
-    //         .pipe(concat('all.html'))
-    //         .pipe(gulp.dest('dist'))
-    //         .pipe(connect.reload()); //发生改变刷新
-    // })
-    // gulp.task('minicss', function () { //压缩并合并css
-    //     gulp.src('')
-    //         .pipe(minifycss())
-    //         .pipe(concat('all.css'))
-    //         .pipe(gulp.dest('dist'))
-    //         .pipe(connect.reload())
-    // })
-    // gulp.task('miniimg', function () { //压缩并合并img
-    //     gulp.src('')
-    //         .pipe(imagemin())
-    //         // .pipe(concat('all.image'))
-    //         .pipe(gulp.dest('dist'))
-    //         .pipe(connect.reload())
-    // })
-    // gulp.task('watch', function () { //实时监听
-    //     gulp.watch('', ['minihtml'])
-    //     gulp.watch('', ['minijs'])
-    //     gulp.watch('', ['minicss'])
-    //     // gulp.watch('', ['miniimg'])
-    // })
-gulp.task('minihtml', function() {
-
-    gulp.src(['rev/js/*.json', 'app/app2/a.html'])
-        .pipe(collector())
-        .pipe(gulp.dest('dist'))
+    gulp.src('app/js/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(rev()) //给压缩后的js文件生成hash字符串后缀
+        // .pipe(concat('all.js')) //合并js
+        .pipe(gulp.dest('dist/js')) //加入dist文件夹内
+        .pipe(rev.manifest()) //生成json文件，进行映射
+        .pipe(gulp.dest('rev/js')) //加入rev/js文件夹内
         .pipe(connect.reload())
 })
-
-
-gulp.task('all', function() {
-    gulp.src('app/**/*.*')
-        .pipe(gulp.dest('dist'))
+gulp.task('miniimg', function() { //压缩并合并img
+    gulp.src('app/images/*.*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
         .pipe(connect.reload())
 })
+gulp.task('minifont', function() { //压缩并合并img
+    gulp.src('app/font/*.*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/font'))
+        .pipe(connect.reload())
+})
+gulp.task('minifont2', function() { //压缩并合并img
+    gulp.src('app/font2/*.*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/font2'))
+        .pipe(connect.reload())
+})
+gulp.task('watch', function() { //实时监听
+    gulp.watch('app/js/*.js', ['minijs'])
+    gulp.watch('app/*.html', ['minihtml'])
+    gulp.watch('app/images/*.*', ['miniimg'])
+    gulp.watch('app/font/*.*', ['minifont'])
+    gulp.watch('app/font2/*.*', ['minifont2'])
+})
+gulp.task('minihtml', function() { //压缩并合并html
+    gulp.src('app/*.html')
+        .pipe(htmlmin())
+        .pipe(gulp.dest('dist'))
+        .pipe(connect.reload()); //发生改变刷新
+})
+
 gulp.task('connect', function() {
     connect.server({
-        root: 'dist',
-
+        root: 'app',
         port: '8888',
-
         livereload: true
     });
 })
-gulp.task('watch', function() {
-    gulp.watch('app/**/*.*', ['all'])
-})
-gulp.task('default', ['all', 'watch', 'connect'])
-
+gulp.task('default', function() {
+    runSequence(['minijs', 'miniimg', 'minifont', 'minifont2'],
+        'minihtml',
+        'watch',
+        'connect'
+    )
+});
 gulp.task('sass', function() {
     gulp.src('app/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
